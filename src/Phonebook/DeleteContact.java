b3867cbd274f82736a44e72dee1b075eb1b4e534
Ali Raza <aliraza.141@outlook.com>
2025-01-28 00:56:42 +0500

    test branch

--- src/Phonebook/DeleteContact.java
+++ src/Phonebook/DeleteContact.java
@@ -4,48 +4,57 @@ import java.awt.*;
 import java.sql.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class DeleteContact extends JFrame implements ActionListener {
-    String x[] = {"Id", "Name", "Nickname", "Phone", "Mobile", "Email", "Address", "Company", "Position", "Group Name"};
+    String[] x = {"Id", "Name", "Nickname", "Phone", "Mobile", "Email", "Address", "Company", "Position", "Group Name"};
     JButton bt;
-    String y[][] = new String[20][10];
-    int i = 0;
-    int j = 0;
+    ArrayList<String[]> data;
     JTable t;
-    Font f, f1;
+    Font f;
     JLabel l1;
     JTextField tf1;
-    JPanel p1, p2;
+    JPanel p1;
 
     DeleteContact() {
         super("Contact Information");
-        setSize(800,400);
-        setLocation(1,1);
-        f = new Font("MS UI Gothic",Font.BOLD,15);
+        setSize(800, 400);
+        setLocation(1, 1);
+        f = new Font("MS UI Gothic", Font.BOLD, 15);
+
+        data = new ArrayList<>();
         try {
             ConnectionClass obj = new ConnectionClass();
             String q = "SELECT * FROM add_contact";
             ResultSet rest = obj.stm.executeQuery(q);
+
             while (rest.next()) {
-                y[i][j++] = rest.getString("id");
-                y[i][j++] = rest.getString("name");
-                y[i][j++] = rest.getString("nickname");
-                y[i][j++] = rest.getString("phone");
-                y[i][j++] = rest.getString("mobile");
-                y[i][j++] = rest.getString("email");
-                y[i][j++] = rest.getString("address");
-                y[i][j++] = rest.getString("company");
-                y[i][j++] = rest.getString("position");
-                y[i][j++] = rest.getString("group_name");
-                i++;
-                j = 0;
+                String[] row = new String[10];
+                row[0] = rest.getString("id");
+                row[1] = rest.getString("name");
+                row[2] = rest.getString("nickname");
+                row[3] = rest.getString("phone");
+                row[4] = rest.getString("mobile");
+                row[5] = rest.getString("email");
+                row[6] = rest.getString("address");
+                row[7] = rest.getString("company");
+                row[8] = rest.getString("position");
+                row[9] = rest.getString("group_name");
+                data.add(row);
             }
-            t = new JTable(y,x);
+
+            // Convert ArrayList to a 2D array for JTable
+            String[][] y = new String[data.size()][10];
+            for (int i = 0; i < data.size(); i++) {
+                y[i] = data.get(i);
+            }
+
+            t = new JTable(y, x);
             t.setFont(f);
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
+
         JScrollPane sp = new JScrollPane(t);
         l1 = new JLabel("Enter Contact Id for deletion");
         tf1 = new JTextField();
@@ -53,12 +62,14 @@ public class DeleteContact extends JFrame implements ActionListener {
         bt.setBackground(Color.BLACK);
         bt.setForeground(Color.RED);
         bt.addActionListener(this);
+
         p1 = new JPanel();
-        p1.setLayout(new GridLayout(1,3,10,10));
+        p1.setLayout(new GridLayout(1, 3, 10, 10));
         p1.add(l1);
         p1.add(tf1);
         p1.add(bt);
-        setLayout(new BorderLayout(10,10));
+
+        setLayout(new BorderLayout(10, 10));
         add(p1, "South");
         add(sp, "Center");
     }
@@ -68,15 +79,14 @@ public class DeleteContact extends JFrame implements ActionListener {
             int idNo = Integer.parseInt(tf1.getText());
             try {
                 ConnectionClass obj1 = new ConnectionClass();
-                String q0 = "delete from add_contact where Id ='" + idNo + "'";
+                String q0 = "DELETE FROM add_contact WHERE Id ='" + idNo + "'";
                 int res = obj1.stm.executeUpdate(q0);
 
                 if (res == 1) {
                     JOptionPane.showMessageDialog(null, "Contact successfully deleted");
                     this.setVisible(false);
                     new DeleteContact().setVisible(true);
-                }
-                else {
+                } else {
                     JOptionPane.showMessageDialog(null, "Error deleting contact");
                     this.setVisible(false);
                     new DeleteContact().setVisible(true);
@@ -84,7 +94,6 @@ public class DeleteContact extends JFrame implements ActionListener {
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-
         }
     }
 
